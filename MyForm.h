71a6ef28f994ad5a5c74234bdbe29cc3f3d4b27e
Anna <Anna@DESKTOP-4UUBHEI>
2024-03-20 09:40:22 +0100

    Добавьте файлы проекта.

---
+++ MyForm.h
@@ -0,0 +1,187 @@
+#pragma once
+#include<ctime>
+#include<random>
+
+namespace pr3 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::RichTextBox^ richTextBox2;
+	private: System::Windows::Forms::Button^ button2;
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Bookman Old Style", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::Color::DarkGreen;
+			this->label1->Location = System::Drawing::Point(12, 20);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(712, 46);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Ввести 12 дійсних чисел та обчислити кількість додатних і кількість \r\nвід’ємних  "
+				L"елементів послідовності.";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->richTextBox1->Location = System::Drawing::Point(12, 81);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(274, 260);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->Text = L"";
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::richTextBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Ivory;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Bookman Old Style", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::Color::SeaGreen;
+			this->button1->Location = System::Drawing::Point(292, 118);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(191, 61);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Обчислити";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// richTextBox2
+			// 
+			this->richTextBox2->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->richTextBox2->Location = System::Drawing::Point(489, 81);
+			this->richTextBox2->Name = L"richTextBox2";
+			this->richTextBox2->Size = System::Drawing::Size(274, 260);
+			this->richTextBox2->TabIndex = 3;
+			this->richTextBox2->Text = L"";
+			this->richTextBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::richTextBox2_TextChanged);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Ivory;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Bookman Old Style", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->ForeColor = System::Drawing::Color::SeaGreen;
+			this->button2->Location = System::Drawing::Point(292, 263);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(191, 61);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Обчислити";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ControlLightLight;
+			this->ClientSize = System::Drawing::Size(777, 363);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->richTextBox2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->label1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"MyForm";
+			this->Text = L"Практична робота №3";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void richTextBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int n = 0;
+		int p = 0;
+		srand(time(0));
+		for (int i = 0; i < 12; i++) {
+			float a = rand() % 201 - 90 + static_cast<float>(rand()) / RAND_MAX;
+
+			richTextBox1->AppendText(a.ToString("0.00") + "\r\n");
+
+			if (a < 0) {
+				n++;
+			}
+			else if (a > 0)
+			{
+				p++;
+			}
+		}
+		richTextBox2->Text = "Кількість від'ємних чисел: " + n.ToString();
+		richTextBox2->AppendText("\nКількість додатніх чисел: " + p.ToString());
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	richTextBox2->Clear();
+	richTextBox1 -> Clear();
+}
+};
+	};
